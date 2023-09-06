4b804bdc94ea7e4fca028eb4de79382716847d11
YasinArafat <117991006+YasinArafat1@users.noreply.github.com>
2023-09-06 21:09:49 +0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactorialExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter any number to see Factorial of That number: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            int res = 1;
+            
+            for (int i = 1; i <=number; i++)
+            {
+                res = res * i;
+                Console.WriteLine(res);
+            }
+            Console.WriteLine();
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
